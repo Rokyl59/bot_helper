6b0d58e69b7c00a94ac4cb17257ff0e49c97e256
Danil_Krupin <133006108+Rokyl59@users.noreply.github.com>
2024-08-20 13:10:16 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import logging
+
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, \
+    CallbackContext
+from dotenv import load_dotenv
+import os
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+
+def start(update: Update, context: CallbackContext) -> None:
+    user = update.effective_user
+    update.message.reply_markdown_v2(
+        fr'Hi {user.mention_markdown_v2()}\!'
+    )
+
+
+def echo(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(update.message.text)
+
+
+def main() -> None:
+    load_dotenv()
+    TOKEN = os.getenv("TOKEN")
+
+    updater = Updater(TOKEN)
+
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command,
+                                          echo))
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
