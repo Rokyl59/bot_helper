c5674d137a8aba04463d752b4d066e05b58085e4
Danil_Krupin <133006108+Rokyl59@users.noreply.github.com>
2024-08-29 13:29:06 +0500

    add DialogFlow to VK bot.

--- vk_bot.py
+++ vk_bot.py
@@ -4,14 +4,20 @@ from dotenv import load_dotenv
 import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
 
+from dialogflow import dialogflow_response
+
 load_dotenv()
 vk_token = os.getenv('TOKEN_VK')
 
 
 def echo(event, vk_api):
+    project_id = os.getenv('DF_PROJECT_ID')
+    text = event.text
+    session_id = event.user_id
+    response_text = dialogflow_response(project_id, session_id, text)
     vk_api.messages.send(
         user_id=event.user_id,
-        message=event.text,
+        message=response_text,
         random_id=random.randint(1, 1000)
     )
 
