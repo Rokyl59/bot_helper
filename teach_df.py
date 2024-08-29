84ac9c571694a17d93e10c4fd4e79462119824c1
Danil_Krupin <133006108+Rokyl59@users.noreply.github.com>
2024-08-29 12:16:54 +0500

    add DialogFlow learning script

---
+++ teach_df.py
@@ -0,0 +1,25 @@
+import json
+import os
+from dotenv import load_dotenv
+
+from dialogflow import create_intent
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    project_id = os.getenv('DF_PROJECT_ID')
+
+    with open('questions.json', 'r') as file:
+        intents_json = file.read()
+    intents = json.loads(intents_json)
+
+    for display_name, intent in intents.items():
+        training_phrases_parts = intent['questions']
+        message_text = [intent['answer'], ]
+        intent_status = create_intent(
+            project_id,
+            display_name,
+            training_phrases_parts,
+            message_text,
+        )
+        print(intent_status)
