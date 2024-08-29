9a5f22812b1ad66ca9f542abd90c624b62bb9af3
Danil_Krupin <133006108+Rokyl59@users.noreply.github.com>
2024-08-29 11:54:27 +0500

    setup DialogFlow

--- main.py
+++ main.py
@@ -1,47 +1,37 @@
-import logging
-
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, \
-    CallbackContext
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler
+from telegram.ext import Filters, CallbackContext
+
+from dialogflow import dialogflow_response
 
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
-
-logger = logging.getLogger(__name__)
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text('Здравствуйте!')
 
 
-def start(update: Update, context: CallbackContext) -> None:
-    user = update.effective_user
-    update.message.reply_markdown_v2(
-        fr'Hi {user.mention_markdown_v2()}\!'
-    )
+def help_command(update: Update, context: CallbackContext):
+    update.message.reply_text('Help!')
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
-
-
-def main() -> None:
-    load_dotenv()
-    TOKEN = os.getenv("TOKEN")
-
-    updater = Updater(TOKEN)
-
-    dispatcher = updater.dispatcher
-
-    dispatcher.add_handler(CommandHandler("start", start))
-
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command,
-                                          echo))
-
-    updater.start_polling()
-    updater.idle()
+def response(update: Update, context: CallbackContext):
+    project_id = os.getenv('DF_PROJECT_ID')
+    text = update.message.text
+    session_id = update.message.chat_id
+    response_text = dialogflow_response(project_id, session_id, text)
+    update.message.reply_text(response_text)
 
 
 if __name__ == '__main__':
-    main()
+    load_dotenv()
+    tg_bot_api_key = os.getenv('TOKEN_TG')
+    updater = Updater(tg_bot_api_key)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(
+        MessageHandler(Filters.text & ~Filters.command, response)
+    )
+    updater.start_polling()
+    updater.idle()
