2e8560697156edcd19fee2e34e1df94de7efda85
Danil_Krupin <133006108+Rokyl59@users.noreply.github.com>
2024-09-05 22:46:15 +0500

    add argparse

--- teach_df.py
+++ teach_df.py
@@ -1,15 +1,35 @@
 import json
 import os
+import argparse
+from pathlib import Path
 from dotenv import load_dotenv
 
 from dialogflow import create_intent
 
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description='Обучение нейросети бота'
+    )
+    parser.add_argument(
+        '--path',
+        type=Path,
+        default='questions.json',
+        help='путь к JSON-файлу с фразами для обучения нейросети',
+    )
+    args = parser.parse_args()
+    return args
+
+
 if __name__ == '__main__':
     load_dotenv()
     project_id = os.getenv('DF_PROJECT_ID')
+    parsed_arguments = parse_arguments()
+    if not Path.exists(parsed_arguments.path):
+        print('Указанный файл не найден!')
+        exit(0)
 
-    with open('questions.json', 'r') as file:
+    with open(parsed_arguments.path, 'r') as file:
         intents_json = file.read()
     intents = json.loads(intents_json)
 
